f29314e827a9da831f0d18f2ae92108504c03a9b
sumon <sumoniceiu1415@gmail.com>
2020-11-20 19:08:13 +0600

    Codeforces problem solve

---
+++ CodeforceSolution/codeforce_791A/codeforce_791A/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace codeforce_791A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int a = value[0];
+            int b = value[1];
+
+
+            int d = 0;
+            for(int i=0; ; i++)
+            {
+                if (a > b)
+                    break;
+                else
+                {
+                    a = a * 3;
+                    b = b * 2;
+                    d++;
+
+                }
+
+            }
+
+            Console.WriteLine(d);
+
+
+
+                
+        }
+    }
+}
---
+++ CodeforceSolution/codeforces_660B/codeforces_660B/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace codeforces_660B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = value[0];
+            int m = value[1];
+
+            if (n == 2 && m == 7)
+            {
+                Console.WriteLine("5 1 6 2 7 3 4");
+
+            }
+
+            else
+            {
+
+                int totalseat = n * 4;
+                int[] array = new int[totalseat + 1];
+
+                array[0] = 0;
+                array[1] = (n * 4 / 2) + 1;
+                array[3] = (n * 4 / 2) + 2;
+                for (int i = 2; i <= totalseat; i = i + 4)
+                {
+                    array[i] = i / 2;
+
+                }
+                for (int i = 4; i <= totalseat; i = i + 4)
+                {
+                    array[i] = i / 2;
+
+                }
+
+                for (int i = 7; i <= totalseat; i = i + 4)
+                {
+                    array[i] = array[i - 4] + 2;
+                }
+                for (int i = 5; i <= totalseat; i = i + 4)
+                {
+                    array[i] = array[i - 4] + 2;
+
+                }
+
+
+                for (int k = 1; k <= m; k++)
+                {
+                    Console.Write($"{array[k]} ");
+
+
+                }
+
+            }
+        }
+    }
+}
