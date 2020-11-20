f5a0b760fba270508a7263b6154a6c74bb58fac7
sumon <sumoniceiu1415@gmail.com>
2020-11-21 00:48:48 +0600

    added codeforces_43A code

---
+++ CodeforceSolution/Codeforces_43A/Codeforces_43A/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Codeforces_43A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int team = Convert.ToInt32(Console.ReadLine());
+
+            string str1 = Console.ReadLine();
+            string str2=" ";
+
+            int d = 1;
+
+            for(int i=1;i<team;i++)
+            {
+                string str3 = Console.ReadLine();
+
+               // bool b = str1.Equals(str3);
+
+
+
+                if(str1.Equals(str3))
+                {
+                    d++;
+                }
+
+                else
+                {
+                    str2 = str3;
+                }
+            }
+
+            int d2 = team - d;
+
+            if (d > d2)
+            {
+                Console.WriteLine(str1);
+            }
+            else
+                Console.WriteLine(str2);
+
+
+
+        }
+    }
+}
