f53eaba79834f2149080efdca81d1258b5011f26
sumon <sumoniceiu1415@gmail.com>
2020-11-21 22:42:20 +0600

    added codeforce_155A code

---
+++ CodeforceSolution/codeforces_155A/codeforces_155A/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace codeforces_155A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] value = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+
+            int max = value[0];
+            int min = value[0];
+            int d = 0;
+
+            for(int i=1;i<n;i++)
+            {
+                if(value[i]>max)
+                {
+                    d++;
+                    max = value[i];
+                }
+
+                else if(value[i]<min)
+                {
+                    d++;
+                    min = value[i];
+
+                }
+            }
+            Console.WriteLine(d);
+
+
+        }
+    }
+}
