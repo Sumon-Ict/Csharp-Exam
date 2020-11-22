65bbbadbb9810a201074d522b11b63fc6114ad63
sumon <sumoniceiu1415@gmail.com>
2020-11-22 16:08:16 +0600

    added codeforce_1295A code

---
+++ CodeforceSolution/codeforce_1295A/codeforce_1295A/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace codeforce_1295A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int testcase = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=0;i<testcase;i++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                if(n%2==1)
+                {
+                    int m = n - 3;
+
+                    Console.Write("7");
+
+                    for (int j=0;j<m/2;j++)
+                    {
+                      
+                        Console.Write("1");
+                       
+                    }
+                    
+
+                    
+
+                }
+                else
+                {
+                    for(int k=0;k<n/2;k++)
+                    {
+                        Console.Write("1");
+                    }
+                    //Console.WriteLine("1");
+
+                }
+
+                Console.WriteLine();
+
+
+            }
+        }
+    }
+}
