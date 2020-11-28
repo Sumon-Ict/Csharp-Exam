e8ecae5a9afde282d210f8de910c8bc349976779
sumon <sumoniceiu1415@gmail.com>
2020-11-28 17:38:33 +0600

    added codeforce_686A code

---
+++ CodeforceSolution/codeforce_686A/codeforce_686A/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace codeforce_686A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            long[] v = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+
+            long n = v[0];
+            long amount = v[1];
+            int d = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] value = Console.ReadLine().Split(' ');
+
+                string a = value[0];
+
+                int b = int.Parse(value[1]);
+
+                if(string.Equals(a,"+"))
+                {
+                    amount += b;
+                }
+
+                else
+                {
+                    if (amount >= b)
+                        amount -= b;
+                    else
+                        d++;
+                }
+                
+            }
+
+            Console.WriteLine($"{amount} {d}");
+
+
+
+        }
+    }
+}
