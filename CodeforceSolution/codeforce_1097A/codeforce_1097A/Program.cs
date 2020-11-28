dde96956e51c561a245d8f176324a00665bb8ffc
sumon <sumoniceiu1415@gmail.com>
2020-11-28 22:32:28 +0600

    added codeforce_1097A code

---
+++ CodeforceSolution/codeforce_1097A/codeforce_1097A/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace codeforce_1097A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str1 = Console.ReadLine();
+
+            string str2 = Console.ReadLine();
+
+            if (str2.Contains(str1[1])||str2.Contains(str1[0]))
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+
+
+         
+        }
+    }
+}
