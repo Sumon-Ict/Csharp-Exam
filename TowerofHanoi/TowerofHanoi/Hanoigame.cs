3662390011fbd35eba45403050313389ecdffa46
sumon <sumoniceiu1415@gmail.com>
2020-12-04 14:12:13 +0600

    added final project

---
+++ TowerofHanoi/TowerofHanoi/Hanoigame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TowerofHanoi
+{
+    class Hanoigame
+    {
+
+        List<char> li = new List<char>();
+        List<char> li2 = new List<char>();
+
+
+
+        public void movecolordice(List<char>li,List<char>li2)
+        {
+            char ch = li[li.Count - 1];
+            li2.Add(ch);
+            li.RemoveAt(li.Count - 1);
+
+        }
+
+        public void presensituation(int d,List<char>li, List<char>li2)
+        {
+            Console.WriteLine($"try no {d}");
+
+            for(int i=0;i<li.Count;i++)
+            {
+                Console.WriteLine(li[i]);
+
+            }
+            for(int i=0;i<li2.Count;i++)
+            {
+                Console.WriteLine(li2[i]);
+            }
+
+        }
+    }
+}
